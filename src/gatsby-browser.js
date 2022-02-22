f8a04bb64c2494f304a8f02864fe8706d5ddfe5f
Lucio Benini <iiriya@iiriya.com>
2022-02-22 18:36:14 +0100

    Update gatsby-browser.js

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,5 +1,7 @@
 import consent from "./utils/consent"
 
+export { wrapPageElement } from "./gatsby-browser"
+
 export function wrapPageElement({ element, props: { navigate, location } }) {
   consent(() => navigate(location.pathname))
   return element
